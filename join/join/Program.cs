1a4175e3fc0503755e52e9d0985483d4113e1405
zulfuqar1 <zulfugarga@code.edu.az>
2023-03-06 02:15:07 +0400

    custom join

---
+++ join/join/Program.cs
@@ -0,0 +1,13 @@
+﻿string join(string[] arr, string ayırıcı)
+{
+    string cavab = string.Empty;
+	for (int i = 0; i < arr.Length; i++)
+	{
+		cavab = cavab + arr[i];
+		if (i < arr.Length - 1)
+		{
+			cavab =cavab+ ayırıcı;
+		}
+	}
+    return cavab;
+}
